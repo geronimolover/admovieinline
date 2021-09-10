4806fbdb2041f0c77f8320a2cdaddb308524cdb7
geronimolover <84311253+geronimolover@users.noreply.github.com>
2021-09-10 20:59:36 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+            InlineKeyboardButton('🔔Main Chanenl🔔', url=f'https://t.me/ADMOVEIAD')
         ],
         [
             InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
